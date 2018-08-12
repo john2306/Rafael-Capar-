5143479c00c8c02b802a8eb7386026b8f0c25057
john <johnmendozauni@gmail.com>
2018-08-12 17:40:03 -0500

    Commit two

--- routes/econometria.js
+++ routes/econometria.js
@@ -3,8 +3,40 @@ const express = require('express');
 
 module.exports=function(app){
     app.get('/econometria', (req, res, next)=>{
-        res.render('pages/econometria2');
+        var datos ={
+            sesionesleft:[
+                {sesion:1, titulo:'Sesion 1: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1G9_tRxjHaobiNmoyTbqpPZLTk0vTYNws'},
+                {sesion:2, titulo:'Sesion 2: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1WHQaSXwRfF8TR4n1dq2pUcAUSeiftcam'},
+                {sesion:3, titulo:'Sesion 3: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1V_blkNL9iuUGfTBnE-mKZY2cVI_UzzTK'},
+                {sesion:4, titulo:'Sesion 4: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1qrs-CTj3aD8WYjtXqJy9UEAmO6Fx1xFV'}
+            ],
+            sesionesright:[
+                {sesion:5, titulo:'Sesion 5: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1mjV3Wtg5FEljr_AxKcStFcCQ3lI6EXGH'},
+                {sesion:6, titulo:'Sesion 6: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1QnTd0fmD_60iprGpbiPAaetafJtyRhCc'},
+                {sesion:7, titulo:'Sesion 7: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1_zThZDAwCvh9fQFzTMAb2X9UVPJhGUP5'},
+                {sesion:8, titulo:'Sesion 8: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'#'}
+            ],
+            sesiones:[
+                {titulo:'Sesiones', bienvenida:'BIENVENIDA AL CURSO Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever.'}
+             ],  
+             practica:[
+                {titulo:'Práctica y Exámenes', bienvenida:'EXPLICANDO LA IMPORTANCIA DE LAS PRÁCTICAS Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever.'}
+             ],           
+            practicas:[
+                {practica:1, titulo:'Primera Práctica', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1l4jQ1fQrMCPG2WA_d1GyiHV4AagnBTUN'},
+                {practica:2, titulo:'Segunda Práctica', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1l4jQ1fQrMCPG2WA_d1GyiHV4AagnBTUN'},
+                {practica:3, titulo:'Tercera Práctica', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1l4jQ1fQrMCPG2WA_d1GyiHV4AagnBTUN'},
+                {practica:4, titulo:'Cuarta Práctica', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1l4jQ1fQrMCPG2WA_d1GyiHV4AagnBTUN'}
+            ],
+            examenes:[
+                {examen:1, titulo:'Exámenes Parciales', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1l4jQ1fQrMCPG2WA_d1GyiHV4AagnBTUN'},
+                {examen:2, titulo:'Exámenes Finales', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1l4jQ1fQrMCPG2WA_d1GyiHV4AagnBTUN'},                
+                {examen:3, titulo:'Exámenes Sustitutorios', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1l4jQ1fQrMCPG2WA_d1GyiHV4AagnBTUN'}
+            ]
+        };
+        res.render('pages/econometria2', datos);
     });
+
 
     app.get('/general/john', (req, res, next)=>{
         res.render('pages/general', {title: 'John Mendoza '});
--- routes/seriestiempo.js
+++ routes/seriestiempo.js
@@ -3,7 +3,43 @@ const express = require('express');
 
 module.exports=function(app){
     app.get('/seriestiempo', (req, res, next)=>{
-        res.render('pages/seriestiempo');
+        var datos ={
+            sesionesleft:[
+                {sesion:1, titulo:'Sesion 1: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1NNjgd0abvOr6ddWVOkn6i8_z2nqhLm5C'},
+                {sesion:2, titulo:'Sesion 2: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1hG_mr3MZPmcs_9Rsgc88ZdAsCoNAEUCe'},
+                {sesion:3, titulo:'Sesion 3: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1hznjA6M2x4-T6dP6lTW6WP7nrvMCAeQH'},
+                {sesion:4, titulo:'Sesion 4: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1-nxSkaTRW114sMqg-HV67ZXewAxRag0n'},
+                {sesion:5, titulo:'Sesion 5: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1SClO0Wegb8_cVLTl-qn5pcZMi1Pdde8K'},
+                {sesion:6, titulo:'Sesion 6: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1GhZplZ6EZ5hsKEpTzBnaLTs3qUIBfpHQ'}            
+            ],
+            sesionesright:[
+                {sesion:7, titulo:'Sesion 7: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1s2ylW73huPb9AGsdCNvXxIzz2YW8h8Ry'},
+                {sesion:8, titulo:'Sesion 8: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1zpRrxyW1SOanQOzZkqgFukLr9oX9Way0'},
+                {sesion:9, titulo:'Sesion 9: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1K8ftacRfvQrzxnLa7sJ-O0IYZvHwT8TI'},
+                {sesion:10, titulo:'Sesion 10: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1GAIO_Bq0UhvK5N95vYUKWnACPSVPH70W'},
+                {sesion:11, titulo:'Sesion 11: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=1A6IiyI3sKcRT6MlZct-1T16uylXTS0e1'},
+                {sesion:12, titulo:'Sesion 12: Título de la sesión', parrafo:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim INTRODUCCIÓN A LA SESIÓN 1 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', enlace:'https://drive.google.com/open?id=16-TbIeeG52EsYF1UtUUKevmn7J075jhW'}
+            ],
+            sesiones:[
+                {titulo:'Sesiones', bienvenida:'BIENVENIDA AL CURSO Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever.'}
+             ], 
+             practica:[
+                {titulo:'Práctica y Exámenes', bienvenida:'EXPLICANDO LA IMPORTANCIA DE LAS PRÁCTICAS Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever.'}
+             ],            
+            practicas:[
+                {practica:1, titulo:'Primera Práctica', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=164CdDsLdBKu7cxUmoReluUjtFvJu5YBC'},
+                {practica:2, titulo:'Segunda Práctica', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1tzunCGDgMhLDtLrPNDDYjtXLpdZXzD2Q'},
+                {practica:3, titulo:'Tercera Práctica', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1PSfVxZ6NEYzoRT-R278vi0wXboRREqmz'},
+                {practica:4, titulo:'Cuarta Práctica', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1veY22w-PugvW6sDjReimEKGExOubA3EU'}
+            ],
+            examenes:[
+                {examen:1, titulo:'Exámenes Parciales', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1fTRW0rGzd_DrDSPv_yL3NO0FJfiTsrlb'},
+                {examen:2, titulo:'Exámenes Finales', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1yTQDsNVrmtRudBqfxra6KkJoS1BgLoTd'},                
+                {examen:3, titulo:'Exámenes Sustitutorios', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1yaAoSQtfyDKarQthg1piWPuVzzlA-tFU'},
+                {libros:3, titulo:'Libros y notas', temas:'Tema1, Tema2, Tema3, etc.', enlace:'https://drive.google.com/open?id=1qpF_H66xVEjZkb7T1FttiHkOguiqPpEd'}
+            ]
+        };
+        res.render('pages/seriestiempo', datos);
     });
 
     app.get('/general/john', (req, res, next)=>{
